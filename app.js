ec6a3224a15571461e16d04b63abefa786ab0abf
Archer <heffernarcher@gmail.com>
2023-04-18 13:45:45 -0400

    updated to be put request for editing transaction

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log(`we are connected!!!\nServer running at http://localhost:${process.env.PORT}`)
+  console.log(`we are connected!!!\nServer running at http://localhost:${process.env.PORT||3000}`)
 });
 
 
